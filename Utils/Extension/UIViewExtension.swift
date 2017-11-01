373056f4067248b5e824a26ffc400ffc99edc845
Shunya Takahashi <s-takahashi.sra@gmo-click.com>
2017-11-01 17:53:00 +0900

    拡張追加

--- Utils/Extension/UIViewExtension.swift
+++ Utils/Extension/UIViewExtension.swift
@@ -29,4 +29,12 @@ extension UIView {
         layer.mask = maskLayer
     }
     
+    func addChildConstraints(for childView: UIView, insets: UIEdgeInsets = .zero) {
+        childView.translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: childView.topAnchor, constant: insets.top).isActive = true
+        bottomAnchor.constraint(equalTo: childView.bottomAnchor, constant: insets.bottom).isActive = true
+        leadingAnchor.constraint(equalTo: childView.leadingAnchor, constant: insets.left).isActive = true
+        trailingAnchor.constraint(equalTo: childView.trailingAnchor, constant: insets.right).isActive = true
+    }
+    
 }
