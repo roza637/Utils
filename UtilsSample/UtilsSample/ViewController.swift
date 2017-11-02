45260d6b9c3451dd5b4d3de1fce562c6bba89f64
Shunya Takahashi <s-takahashi.sra@gmo-click.com>
2017-11-02 15:15:06 +0900

    動作確認用プロジェクトを追加

---
+++ UtilsSample/UtilsSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UtilsSample
+//
+//  Created by fxgmo on 2017/11/02.
+//  Copyright © 2017年 roza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
